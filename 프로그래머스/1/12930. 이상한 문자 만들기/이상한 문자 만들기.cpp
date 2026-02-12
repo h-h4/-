d421df5b1560dce9c4cb4caebcec22f7c0ac5db4
h-h4 <raphi331110@gmail.com>
2026-02-13 08:56:42 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+string solution(string s) {
+    int index = 0;
+    
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            
+            index = 0;
+            continue;
+        }
+        
+        if (index % 2 == 0) {
+            s[i] = toupper(s[i]);
+        } else {
+            s[i] = tolower(s[i]);
+        }
+        index++;
+    }
+    
+    return s;
+}
